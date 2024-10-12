ea3df650e5ac6d5ded71f07c9d982434216f37ce
Mr-Tea-Baggins <officecakes@proton.me>
2024-10-13 01:01:53 +0200

    Update pre-suspend.sh

--- pre-suspend.sh
+++ pre-suspend.sh
@@ -4,5 +4,5 @@ if [ "${1}" == "pre" ]; then
 	echo "nothing here..."
 elif [ "${1}" == "post" ]; then
 	sleep 1s
-	bluetoothctl connect 
+	bluetoothctl connect #REPLACE THIS COMMENTED TXT WITH YOUR BLUETOOTH SPEAKER MAC ADDRESS
 fi
